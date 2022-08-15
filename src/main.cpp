521c1d1186a7b2c453f1cba2b63a34a51bb3cb6d
farinellizin <lucasfcpinho@gmail.com>
2022-08-15 19:13:02 -0300

    Adding initial files.

---
+++ src/main.cpp
@@ -0,0 +1,6 @@
+#include "matrix.hpp"
+using namespace std;
+
+int main() {
+    
+}
---
+++ src/matrix.hpp
@@ -0,0 +1,10 @@
+#ifndef MATRIX_HPP
+#define MATRIX_HPP
+using namespace std;
+#include <iostream>
+#include <fstream>
+#include <string>
+
+
+
+#endif
