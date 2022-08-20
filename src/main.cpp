14fe030103626b3a6ddfe04f7e8af72d3c0df99c
farinellizin <lucasfcpinho@gmail.com>
2022-08-20 03:57:21 -0300

    Logic issues fixed.

--- src/main.cpp
+++ src/main.cpp
@@ -2,33 +2,27 @@
 using namespace std;
 
 int main() {
-    int initial_i = 18, initial_j = 15, final_i = 20, final_j = 20;
-
-    // cout << "\t- Please inform the initial I coordenate: ";
-    // cin >> initial_i;
-
-    // cout << "\t- Please inform the initial J coordenate: ";
-    // cin >> initial_j;
-
-    // cout << "\t- Please inform the final I coordenate: ";
-    // cin >> final_i;
-
-    // cout << "\t- Please inform the final J coordenate: ";
-    // cin >> final_j;
-
+    int initial_i, initial_j, final_i, final_j;
     ifstream myfile;
     myfile.open("matrix.txt");
-
     string line;
-
-    int line_cont = (final_i - initial_i + 1), cont = 0;
-
+    int cont = 0;
+    int num_elements = (final_i - initial_i + 1) * (final_j - initial_j + 1);
     int matrix[final_i - initial_i + 1][final_j - initial_j + 1];
-    cout << "[ " << final_i - initial_i + 1 << " ][ " << final_j - initial_j + 1 << " ]\n\n"; 
-
     int j_coord = 0;
-
     int I = 0, J = 0;
+
+    cout << "\t- Please inform the initial I coordenate: ";
+    cin >> initial_i;
+
+    cout << "\t- Please inform the initial J coordenate: ";
+    cin >> initial_j;
+
+    cout << "\t- Please inform the final I coordenate: ";
+    cin >> final_i;
+
+    cout << "\t- Please inform the final J coordenate: ";
+    cin >> final_j;
 
     if (myfile.is_open()) {
         for(int i = 0; i < initial_i - 1; i++) {
@@ -36,25 +30,19 @@ int main() {
         }
 
         while(!myfile.eof()) {
-            while (getline(myfile, line, ' ')) {
+            while (getline(myfile, line, ' ') && cont < num_elements) {
                 j_coord++;
 
                 if (j_coord >= initial_j && j_coord <= final_j) {
                     matrix[I][J] = stoi(line);
-                    cout << "[" << I << "][" << J + 1 << "] = " << matrix[I][J] << endl;
-                    cout << endl << endl << "Line Cont = " << line_cont << " cont = " << cont << endl << endl;
-
                     J++;
                 
                     if (J == (final_j - initial_j + 1)) {
                         J = 0;
                         I++;
-                        cont++;
                     }
 
-                    if(cont == line_cont) {
-                        break;
-                    }
+                    cont++;
                 }
 
                 if (j_coord == final_j) {
@@ -68,7 +56,6 @@ int main() {
         for (int j = 0; j < (final_j - initial_j + 1); j++) {
             cout << matrix[i][j] << " ";
         }
-
         cout << endl;
     }
 }
